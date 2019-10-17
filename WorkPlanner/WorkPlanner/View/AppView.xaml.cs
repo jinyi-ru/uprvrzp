f89f2f2909b8186ea09ee491ce5055f801da9b00
jinyi-ru <56396454+jinyi-ru@users.noreply.github.com>
2019-10-17 14:49:49 +0300

    Добавлены файлы

--- WorkPlanner/WorkPlanner/View/AppView.xaml.cs
+++ WorkPlanner/WorkPlanner/View/AppView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WorkPlanner
+namespace WorkPlanner.View
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для AppView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AppView : Window
     {
-        public MainWindow()
+        public AppView()
         {
             InitializeComponent();
         }
